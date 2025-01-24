6bbe9077356cc0ea5fa1fc097d29e2ea1722e3d4
Art <ya.Artriches@ya.ru>
2025-01-24 20:55:01 +0300

    feature: настроил сборку

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,22 @@
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
 plugins {
+    application
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
 }
 
-group = "hexlet.code"
-version = "1.0-SNAPSHOT"
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
